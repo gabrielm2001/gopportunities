2c84e001ee80180584f5b3e9470d8011bac66a17
Gabriel Machado <gabriel.machado@bemobi.com>
2025-07-14 14:01:13 -0300

    Feat: Add routes logic

--- router/router.go
+++ router/router.go
@@ -5,11 +5,8 @@ import "github.com/gin-gonic/gin"
 
 func Initialize(){
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	InitializeRoutes(router)
 
 	router.Run(":8080")
 }
